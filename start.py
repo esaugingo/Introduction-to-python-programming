261d5f786e1b1263244d9beb4f29a06b2e89c504
esau.jingo <esau@microhaem.co.ug>
2021-04-12 13:45:30 +0300

    First assignment

---
+++ start.py
@@ -0,0 +1,10 @@
+# Enter first and last name
+names = input("Enter first and last name: \n") 
+
+print()
+
+#enter your goals for this class
+goals = input("what are your goals for this class \n")
+print ()
+print('thank you ')
+
